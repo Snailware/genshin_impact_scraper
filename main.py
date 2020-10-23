5e4617a2c6628406d6e40f9f207b267774ae33dd
Adaml <Adamlancaster.al@gmail.com>
2020-10-23 11:17:15 -0400

    successfully tested.

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ class Scraper:
 # scraper object.
 
     def __init__(self, url):
-        self.base_url = url
-        self.weapon_type_list = []
-        self.weapon_list = []
+        self.base_url           = url
+        self.weapon_type_list   = []
+        self.weapon_list        = []
+        self.character_list     = []
     # initialize variables. 
 
     def scrapeTypes(self):
@@ -56,7 +57,7 @@ class Scraper:
     # scrape weapon info, format entries for list and remove junk. 
 
     def outputWeapons(self):
-        tsv_weapon_file = open("weapon_list.tsv", "a")
+        tsv_weapon_file = open("weapon_list.tsv", "w")
         tsv_weapon_file.write("name\tweapon type\trarity\tbase attack\t" +
         "2nd stat\tpassive\trank 1\trank 5\n")
         for weapon in self.weapon_list:
@@ -65,6 +66,37 @@ class Scraper:
             f"{weapon[7]}\n")
         tsv_weapon_file.close()
     # write table TSV file for viewing / export.
+
+    def scrapeCharacters(self):
+        request_timer = Timer()
+        request_timer.start()
+        print("requesting Character information...")
+
+        page = requests.get(f"{self.base_url}/Characters")
+        print(f"received Character info in approx {request_timer.stop()} seconds.")
+
+        parsed_page = BeautifulSoup(page.content, "html.parser")
+        table = parsed_page.find_all(class_= "article-table")
+        rows = table[1].find_all("tr")
+        for row in rows:
+            character_entry = []
+            cells = row.find_all("td")
+            for data in cells:
+                if data.text.strip() != "":
+                    character_entry.append(data.text.strip())
+            if character_entry != []:
+                if character_entry[1] == "Traveler":
+                    character_entry.append("None")
+                self.character_list.append(character_entry)
+    # scrape character info from wiki. 
+
+    def outputCharacters(self):
+        tsv_character_file = open("character_list.tsv", "w")
+        tsv_character_file.write("name\trarity\tgender\tweapon\telement\tnation\n")
+        for character in self.character_list:
+            tsv_character_file.write(f"{character[1]}\t{character[0]}\t{character[4]}\t{character[3]}\t{character[2]}\t{character[5]}\n")
+        tsv_character_file.close()
+    # output character info to character list TSV file. 
 
 class Timer:
 # timer object.
@@ -92,10 +124,16 @@ def main():
     # start timer and alert user. 
     
     scraper = Scraper("https://genshin-impact.fandom.com/wiki")
+    # create scraper instance.
+
     scraper.scrapeTypes()
     scraper.scrapeWeapons()
     scraper.outputWeapons()
-    # scrape weapon information and output to TSV file. 
+    # scrape weapon information and output to TSV file.
+     
+    scraper.scrapeCharacters()
+    scraper.outputCharacters()
+    # scrape character information and output to TSV file. 
 
     print(f"execution finished successfully in approx {timer.stop()} seconds.")
     exit()
